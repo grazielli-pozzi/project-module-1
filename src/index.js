025c2c758c4bf9ebed918545ada2603293afe9dc
Grazielli Pozzi <graziellipozzi@gmail.com>
2020-08-09 15:32:56 -0300

    Version-1

---
+++ src/index.js
@@ -0,0 +1,52 @@
+//Quando a pessoa clicar no botão JOGAR, limpar a section e adicionar o jogo
+
+class Game {
+    constructor() {
+
+    }
+
+    checkPlayerTurn() {
+
+    }
+
+    showWinner() {
+
+    }
+
+}
+
+class Player {
+    constructor() {
+
+    }
+
+    checkPiecesPossibles() {
+
+    }
+
+    getNewPieces() {
+
+    }
+
+}
+
+class Piece {
+    constructor() {
+
+    }
+
+    selectInitialSetOfPieces() {
+
+    }
+
+    selectNewPiece() {
+        
+    }
+
+    movePiece() {
+
+    }
+
+
+
+}
